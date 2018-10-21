1e421d560e0be50c6e140e834e4b729aa5c56b10
Martin Persson <mnpn03@gmail.com>
2018-10-21 18:05:20 +0200

    20/20/20: Less verbose

--- ubuntu-20-20-20.rb
+++ ubuntu-20-20-20.rb
@@ -12,6 +12,6 @@ while true do # Infinite loop
 	sleep 1200 # Sleep for 20 minutes
 	`notify-send "20/20/20" "Look at an object 6 metres away from you until you hear a chime."` # Send a notification
 	sleep 25 # Sleep for 20+5 seconds (give the user a few seconds)
-	system "ffplay /usr/share/sounds/gnome/default/alerts/sonar.ogg -autoexit" # Play some default Ubuntu sound.
+	system "ffplay -loglevel panic /usr/share/sounds/gnome/default/alerts/sonar.ogg -autoexit" # Play some default Ubuntu sound.
 	`notify-send "20/20/20" "Well done!"` # Congratulate the user.
 end
